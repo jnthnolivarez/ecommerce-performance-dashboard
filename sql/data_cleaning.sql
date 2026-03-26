384a838aa56fd8a34e3fa9eee270b410254938c6
Jonathan Olivarez <143167626+jnthnolivarez@users.noreply.github.com>
2026-03-26 13:02:29 +0800

    Update data_cleaning.sql

--- sql/data_cleaning.sql
+++ sql/data_cleaning.sql
@@ -1,43 +1,37 @@
--- Analyze P&L
-SELECT 
-    SUM(oi.price * oi.quantity) AS total_revenue,
-    SUM(oi.cost * oi.quantity) AS total_cost,
-    SUM(oi.price * oi.quantity) - SUM(oi.cost * oi.quantity) AS profit
-FROM orders o
-JOIN order_items oi
-ON o.order_id = oi.order_id;
+--- Data Cleaning
 
---- Customer Acquisition Cost (CAC) how much it costs to get 1 customer.
+SELECT *
+FROM orders
+WHERE amount IS NULL;
 
-SELECT channel, SUM(ad_spend) / SUM(new_customers) AS CAC
-FROM marketing_spend
-GROUP BY channel
+-- Replace NULL amount with 0 
+UPDATE orders
+SET amount = 0
+WHERE amount IS NULL;
 
---- CAC Vs Revenue
+-- Check for NULL price or cost
+SELECT *
+FROM order_items
+WHERE price IS NULL OR cost IS NULL;
 
-WITH revenue_data AS (
-	SELECT 
-		o.channel,
-    	SUM(oi.price * oi.quantity) AS total_revenue,
-    	SUM(oi.cost * oi.quantity) AS total_cost,
-    	SUM(oi.price * oi.quantity) - SUM(oi.cost * oi.quantity) AS profit
-	FROM orders o
-	JOIN order_items oi
-	ON o.order_id = oi.order_id
-	GROUP BY o.channel
-),
-cac_data AS (
-	SELECT 
-		channel, 
-		SUM(ad_spend) / SUM(new_customers) AS CAC
-	FROM marketing_spend
-	GROUP BY channel
-)
-SELECT
-	r.channel,
-	r.total_revenue,
-	r.profit,
-	c.CAC
-FROM revenue_data r
-JOIN cac_data c
-ON r.channel = c.channel
+-- Replace NULL values with 0
+UPDATE order_items
+SET price = 0
+WHERE price IS NULL;
+
+UPDATE order_items
+SET cost = 0
+WHERE cost IS NULL;
+
+SELECT order_id, product_id, COUNT(*)
+FROM order_items
+GROUP BY order_id, product_id
+HAVING COUNT(*) > 1;
+
+-- Keep only one record
+DELETE FROM order_items
+WHERE ctid NOT IN (
+    SELECT MIN(ctid)
+    FROM order_items
+    GROUP BY order_id, product_id
+);
